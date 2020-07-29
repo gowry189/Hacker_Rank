3dcc924167b7c0abd30f19c7b58aa088880a1610
Gowry Sugathan <38757089+gowry189@users.noreply.github.com>
2020-07-29 18:12:32 +0530

    Add files via upload

---
+++ evenorodd.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+cout<<"Enter the Number to check";
+cin>>n;
+if(n==0){
+	return 0;
+}
+else if(n%2==0){
+	cout<<"Yes";
+}
+else{
+	cout<<"No";
+}
+}
