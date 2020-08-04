5ad7d45ff3ac4586f7afd1e002d520a4f062c5a1
Gowry Sugathan <38757089+gowry189@users.noreply.github.com>
2020-08-04 23:25:31 +0530

    Longest Palindrome Subsequence Length

---
+++ longestPali.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    string s;
+    cin>>s;
+    int n = s.length();
+    int a[n][n];
+    int i,k,j;
+    for(i=0;i<n;i++){
+        a[i][i]=1;
+    }
+    for(k=2;k<=n;k++){
+        for(i=0;i<n-k+1;i++){
+            j=i+k-1;
+            if(s[i]==s[j]&& k==2){
+                a[i][j]=2;
+            }
+            else if(s[i]==s[j]){
+                a[i][j]=a[i+1][j-1]+2;
+            }
+            else{
+                a[i][j]=max(a[i][j-1],a[i+1][j]);
+            }
+        }
+    }
+    cout<<a[0][n-1];
+    return 0;
+}
+
