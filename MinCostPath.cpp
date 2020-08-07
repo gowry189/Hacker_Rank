3be7ea3e8663f17be4fb71292279435a4e77ed83
Gowry Sugathan <38757089+gowry189@users.noreply.github.com>
2020-08-07 23:09:01 +0530

    Minimum cost path

---
+++ MinCostPath.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+int min(int a,int b,int c){
+	return (a<b && a<c )? a: b<c ? b: c ;
+}
+int main(){
+	int n,m;
+	cin>>n>>m;
+	int arr[n][m];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			cin>>arr[i][j];
+		}
+	}
+	int dp[n][m];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			dp[i][j]=0;
+		}
+	}
+	dp[0][0]=arr[0][0];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			if(i==0 && j!=0){
+				dp[i][j]=arr[i][j]+dp[i][j-1];
+			}
+			else if(j==0 && i!=0){
+				dp[i][j]=arr[i][j]+dp[i-1][j];
+			}
+			else{
+				dp[i][j]=min(dp[i][j-1],dp[i-1][j-1],dp[i-1][j])+arr[i][j];
+			}
+		}
+	}
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			cout<<dp[i][j];
+		}
+	cout<<"\n";
+	}
+	return 0;
+}
