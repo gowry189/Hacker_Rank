ae24537c74ca47f47be60dfec33982105824a2ad
Gowry Sugathan <38757089+gowry189@users.noreply.github.com>
2020-08-07 23:22:36 +0530

    Update MinCostPath.cpp

--- MinCostPath.cpp
+++ MinCostPath.cpp
@@ -13,11 +13,6 @@ int main(){
 		}
 	}
 	int dp[n][m];
-	for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++){
-			dp[i][j]=0;
-		}
-	}
 	dp[0][0]=arr[0][0];
 	for(int i=0;i<n;i++){
 		for(int j=0;j<n;j++){
@@ -27,16 +22,18 @@ int main(){
 			else if(j==0 && i!=0){
 				dp[i][j]=arr[i][j]+dp[i-1][j];
 			}
-			else{
+			else if(i!=0 && j!=0){
 				dp[i][j]=min(dp[i][j-1],dp[i-1][j-1],dp[i-1][j])+arr[i][j];
 			}
 		}
 	}
+	cout<<"\n";
 	for(int i=0;i<n;i++){
 		for(int j=0;j<m;j++){
-			cout<<dp[i][j];
+			cout<<dp[i][j]<<" ";
 		}
 	cout<<"\n";
 	}
+	cout<<"The minimum cost "<<dp[n-1][m-1];
 	return 0;
 }
