ecd8b9ea10f798f1a542e95b83007c55efd95335
Gowry Sugathan <38757089+gowry189@users.noreply.github.com>
2020-08-17 23:21:46 +0530

    Solve diagonal difference

---
+++ diagonalDifference.cpp
@@ -0,0 +1,27 @@
+/*https://www.hackerrank.com/challenges/diagonal-difference/problem*/
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n][n];
+    int sum1=0,sum2=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+          cin>>arr[i][j];
+
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j){
+                sum1+=arr[i][j];
+            }
+            if(i+j == n-1){
+                sum2+=arr[i][j];
+            }
+        }
+    }
+    cout<<abs(sum1-sum2);
+    return 0;
+}
